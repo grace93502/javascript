99ec773840db1b09560c14546ccffdaf41ca8638
grace93502 <dygrace9350@outlook.com>
2024-08-20 16:05:29 +0900

    Create 1.variable.js

---
+++ 1.variable.js
@@ -0,0 +1,13 @@
+// 상수
+const age = 20;
+// 변수
+let grade = 3;
+let year;
+year = 4;
+year = 5;
+
+// console.log 
+// 변수의 내용을 출력하는 함수 
+console.log(age);
+console.log('나이는 ' + age + '살이고 학년은 ' + grade + '학년이다.');
+console.log(`나이는 ${age}살이고 학년은 ${grade}학년이다.`); //이 경우는 작은 따옴표가 아니라 ` 임
